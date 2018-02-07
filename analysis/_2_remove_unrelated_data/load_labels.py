bbc032b33006ef2bbc506495cdb957a89c31704d
Wong Jia Hau <hou32hou@gmail.com>
2018-02-07 13:27:29 +0800

    Fix process of step 2

--- analysis/_2_remove_unrelated_data/load_labels.py
+++ analysis/_2_remove_unrelated_data/load_labels.py
@@ -1,7 +1,9 @@
 import json
+from analysis.log import log
 
 
 def load_labels(file_path, language):
+    log(f"Loading {language} labels from {file_path}", 1)
     result = []
     with open(file_path, encoding='utf8') as file:
         dic = json.load(file)
@@ -16,5 +18,6 @@ def load_labels(file_path, language):
                 result += dic[key]['alias_cn']
             else:
                 raise Exception("Invalid language argument. Expected (chinese/english) but got " + language)
+    log(f"{len(result)} {language} labels loaded", 2)
     return result
 
--- analysis/_2_remove_unrelated_data/main.py
+++ analysis/_2_remove_unrelated_data/main.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 from hanziconv import HanziConv
 from analysis.log import log
 from analysis._2_remove_unrelated_data.load_labels import load_labels
-from analysis._2_remove_unrelated_data.load_labels import flatten
 from analysis._2_remove_unrelated_data.label_post import label_post
 from analysis.load_posts import load_posts
 from analysis.save_posts import save_posts
@@ -16,22 +15,23 @@ def main(language):
     label_post(posts, labels)
     log(f"Removing unrelated posts", 1)
     purified = [x for x in posts if len(x['related_to']) > 0]
-    dumped = [x for x in posts if len(x['related_to']) <= 0]
     log(f"Number of removed posts = " + str(len(posts) - len(purified)), 1)
     save_posts(purified, f'analysis/_2_remove_unrelated_data/{language}.json')
-    # save_posts(
-    #     dumped, f'analysis/_2_remove_unrelated_data/dumped_{language}.json')
+    SAVE_DUMPED_POST = False
+    if SAVE_DUMPED_POST:
+        dumped = [x for x in posts if len(x['related_to']) <= 0]
+        save_posts(
+            dumped, f'analysis/_2_remove_unrelated_data/dumped_{language}.json')
 
 
 def get_labels(language):
-    log(f"Loading {language} labels", 1)
     labels_dir = 'keywords/target/'
-    leaders = load_labels(labels_dir + f'{language}_leader.txt')
-    parties = load_labels(labels_dir + f'{language}_party.txt')
+    leaders = load_labels(labels_dir + f'leader.json', language)
+    parties = load_labels(labels_dir + f'party.json', language)
     if language == 'english':
-        return flatten(leaders + parties)
+        return (leaders + parties)
     elif language == 'chinese':
-        simplified = list(map(HanziConv.toSimplified, flatten(leaders + parties)))
+        simplified = list(map(HanziConv.toSimplified, (leaders + parties)))
         return list(OrderedDict.fromkeys(simplified))
 
 
