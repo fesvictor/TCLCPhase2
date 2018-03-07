34507685fc719ab3041c93a8deb53ac06c7c4039
Wong Jia Hau <hou32hou@gmail.com>
2018-03-07 09:36:09 +0800

    Added code for saving the predicted output using fasttext model

---
+++ analysis/using_fasttext/extract_semantic_label.py
@@ -0,0 +1,12 @@
+from analysis.load_posts import load_posts
+from analysis.save_posts import save_posts
+
+
+posts = load_posts(f'analysis/using_fasttext/labelled_english_posts.json')
+positive = "__label__2"
+with open('analysis/using_fasttext/predicted_label.txt') as file:
+    labels = file.read().split('\n')
+    for i in range(0, len(posts)):
+        posts[i]["semantic_value"][("positive" if labels[i] == positive else "negative")] = True
+
+save_posts(posts,'analysis/results/fasttext/english_analyzed.json' )
