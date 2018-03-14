162d424c13ff2af40946570e7cc7fe5fb8d7d419
Wong Jia Hau <hou32hou@gmail.com>
2018-03-14 09:33:40 +0800

    Excluded facebook post message, now it will extract comments only

--- analysis/_1_process_raw_data/parse_facebook_json.py
+++ analysis/_1_process_raw_data/parse_facebook_json.py
@@ -2,11 +2,17 @@ import json
 import ntpath
 from analysis.libs.Post import Post
 
-# ASSUMPTIONS:
-# - I assume that data without the key "message" will have the key "story"
-# - I also assume that data with key "story" contain meaningless message
-# - e.g. MalaysiaKini have posted a photo
-# Thus, I will ignore data that contain key "story"
+"""
+ASSUMPTIONS:
+- I assume that data without the key "message" will have the key "story"
+- I also assume that data with key "story" contain meaningless message
+- e.g. MalaysiaKini have posted a photo
+Thus, I will ignore data that contain key "story".
+
+According to Dr. Victor, he said since post message will be neutral,
+we will not get its value, so we will only extract comments at the moment
+"""
+EXTRACT_POST_MESSAGE = False
 
 def parse_facebook_json(file_path):
     result = []
@@ -22,7 +28,7 @@ def translate_data_to_post(data_list, file_path):
     if data_list is None:
         return result
     for data in data_list:
-        if "message" in data:
+        if "message" in data and EXTRACT_POST_MESSAGE:
             p = Post()
             p.date = data["created_time"]
             p.value = str.lower(data["message"])
