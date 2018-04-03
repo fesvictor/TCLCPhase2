bcb1fc0d216fd80dfb25864afd2bbc281c55a679
cshong0618 <cshong.gcc@hotmail.com>
2018-04-03 11:12:49 +0800

    Updated graphs : 3 Apr 2018

--- scraping/facebook_scraper/main_get_comment_count.py
+++ scraping/facebook_scraper/main_get_comment_count.py
@@ -2,8 +2,10 @@ from fb import FacebookScraper
 import json
 import pathlib
 import os
+import notify2
 
 if __name__ == "__main__":
+    notify2.init("Facebook scrapper")
     token = ""
     with open('params.json') as fin:
         obj = json.loads(fin.read())
@@ -15,21 +17,22 @@ if __name__ == "__main__":
 
     # facebook date format 2018-03-03T03:01:00+0000 YYYY-MM-DDTHH:mm:ss+0000 <-- UTC time, have to +8 for local time
 
-
+    output = {}
     print("From: %s   To: %s" % (start_date, end_date))
-    output = {'data': {}}
     for page in pages:
         fs = FacebookScraper(token)
         fs.get_posts([page], start_date, end_date, verbose=True)
-        fs.get_comments_count(verbose=True)
+        fs.get_levelled_comments_count(verbose=True)
 
-        print("%d posts scraped" % len(fs.posts))
-        output['data'][fs.name] = {}
-        output['data'][fs.name]['data'] = fs.posts
-        output['data'][fs.name]['comment_count'] = fs.total_comment_count
+        print("%d posts scraped" % len(fs.posts_list))
+        output[fs.name] = {'data': fs.comment_count}
 
         output_filename = "fb__%s__to__%s__count.json" % (start_date, end_date)
 
         pathlib.Path('data').mkdir(parents=True, exist_ok=True)
         with open(os.path.join('data', output_filename), "w") as fout:
             fout.write(json.dumps(output))
+
+        
+        # Bring over token
+        token = fs.access_token
